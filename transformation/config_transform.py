072fd2fc1bbce39a527fa116f9ed86b0d48b7e84
Francesco Infante <fra.inf@icloud.com>
2015-07-09 17:10:57 +0200

    Added ConfigTransform

--- transformation/config_transform.py
+++ transformation/config_transform.py
@@ -1,6 +1,25 @@
 __author__ = 'Francesco Infante'
 
+import dpath.util
+
 from api import Transformation
+
+
+class Configuration(object):
+    def __init__(self, **rules):
+        self.rules = rules
+
+
+class Call(object):
+    def __init__(self, function, *args, **kwargs):
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+
+
+class Path(object):
+    def __init__(self, path):
+        self.path = path
 
 
 class ConfigTransform(Transformation):
@@ -12,9 +31,20 @@ class ConfigTransform(Transformation):
         self.source = source
         self.config = config
 
-    def transform(self, data):
-        # TODO
-        pass
+    def _transform(self, data, config=None):
+        if config is None:
+            config = self.config
+
+        if isinstance(config, Configuration):
+            return {k: self._transform(data, v) for k, v in config.rules.iteritems()}
+        elif isinstance(config, Call):
+            args = [self._transform(data, v) for v in config.args]
+            kwargs = {k: self._transform(data, v) for k, v in config.kwargs.iteritems()}
+            return config.function(*args, **kwargs)
+        elif isinstance(config, Path):
+            return dpath.util.get(data, config.path)
+
+        return config
 
     def next(self):
-        return self.transform(self.source.next())
+        return self._transform(self.source.next())
