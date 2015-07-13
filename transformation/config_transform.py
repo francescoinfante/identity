ab80f6c728889086e02afb2a5cc383211fadd55e
Francesco Infante <fra.inf@icloud.com>
2015-07-13 17:25:11 +0200

    handle keyerror dpath

--- transformation/config_transform.py
+++ transformation/config_transform.py
@@ -22,7 +22,10 @@ def _transform(args):
         kwargs = {k: _transform((data, v)) for k, v in config.kwargs.iteritems()}
         result = config.function(*args, **kwargs)
     elif isinstance(config, Path):
-        result = dpath.util.get(data, config.path)
+        try:
+            result = dpath.util.get(data, config.path)
+        except KeyError:
+            result = None
 
     return result
 
