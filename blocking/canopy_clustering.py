d47b702b2f0a47518f7316ad5a4040d5bbc9e7e3
Francesco Infante <fra.inf@icloud.com>
2015-08-13 09:31:05 +0200

    Added blocking algorithm classes to implement

---
+++ blocking/canopy_clustering.py
@@ -0,0 +1,11 @@
+__author__ = 'Francesco Infante'
+
+from api import Blocking
+
+
+class CanopyClustering(Blocking):
+    def __init__(self, source):
+        raise NotImplementedError()
+
+    def next(self):
+        raise NotImplementedError()
---
+++ blocking/min_hash.py
@@ -0,0 +1,11 @@
+__author__ = 'Francesco Infante'
+
+from api import Blocking
+
+
+class MinHash(Blocking):
+    def __init__(self, source):
+        raise NotImplementedError()
+
+    def next(self):
+        raise NotImplementedError()
