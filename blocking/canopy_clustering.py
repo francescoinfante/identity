dee8521175324ff6ca767a093cb2afaec0a3d7e8
Francesco Infante <fra.inf@icloud.com>
2015-08-13 16:07:42 +0200

    added comment

--- blocking/canopy_clustering.py
+++ blocking/canopy_clustering.py
@@ -4,8 +4,19 @@ from api import Blocking
 
 
 class CanopyClustering(Blocking):
-    def __init__(self, source):
-        raise NotImplementedError()
+    """
+    Algorithm based on McCallum et al. (2000).
+
+    It takes as arguments the documents, the distance metric (instance of Feature) and the thresholds t1 and t2.
+
+    The distance metric must give a numeric value.
+
+    It returns all the pairs within a canopy (Non-disjoint blocking).
+    """
+
+    def __init__(self, source, distance, t1, t2):
+
+    # TODO
 
     def next(self):
-        raise NotImplementedError()
+        return self._pairs.next()
