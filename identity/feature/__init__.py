b1b62ccf757dd191f884d7fa81944482d4f438c0
Francesco Infante <fra.inf@icloud.com>
2015-08-29 15:36:06 +0200

    fix feature extraction + example

--- identity/feature/__init__.py
+++ identity/feature/__init__.py
@@ -1,30 +1,50 @@
 __author__ = 'Francesco Infante'
 
+from inspect import isclass
+
 from string_metrics import Levenshtein
+from identity.common import extract_from_tuple, Path
 
 
 class FeatureExtraction(object):
     """
     Args:
         source ([tuple]): list of pairs of records
-        features ([Feature]): list of features to extract from each pair
+        features ([(Feature, Path)]): list of features to extract from each pair from the given path
     """
 
     def __init__(self, pairs, features):
-        self.pairs = pairs
+        self.pairs = iter(pairs)
         self.features = features
+
+    def __iter__(self):
+        return self
 
     def next(self):
         pair = self.pairs.next()
         features_vector = {}
-        for feat in self.features:
-            result = feat.extract(pair)
+        for feat, path in self.features:
+            if isclass(feat):
+                feat = feat()
+            e = extract_from_tuple(pair, path)
+            result = feat.extract(e[0], e[1])
             if isinstance(result, dict):
-                features_vector.update({feat.prefix() + ':' + str(name): value for name, value in result.iteritems()})
+                features_vector.update({feat.__class__.__name__ + '@' + path + ':' + str(name): value for name, value in
+                                        result.iteritems()})
             else:
-                features_vector[feat.prefix()] = result
+                features_vector[feat.__class__.__name__ + '@' + path] = result
         return features_vector
 
 
 if __name__ == "__main__":
-    pass
+    sample = [({'id': 3, 'title': 'matrix',
+                'actors': ['keanu reeves', 'laurence fishburne', 'carrie-anne moss']},
+               {'id': 5, 'title': 'matrix',
+                'actors': ['keanu reeves', 'laurence fishburne', 'carrie-anne moss']}),
+              ({'id': 8, 'title': 'la vita e bella'},
+               {'id': 11, 'title': 'la vita e bella'})]
+
+    feats = [(Levenshtein, Path('title'))]
+
+    for x in FeatureExtraction(sample, feats):
+        print x
